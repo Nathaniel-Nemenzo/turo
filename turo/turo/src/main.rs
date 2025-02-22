8a3e9e14d12ceaeddf245dbec0608bd3de852c63
Nathaniel Nemenzo <nathanielnemenzo2@gmail.com>
2025-02-22 17:51:14 -0600

    [misc] updating dependencies

--- turo/turo/src/main.rs
+++ turo/turo/src/main.rs
@@ -13,30 +13,34 @@
 
 use turo::{serial_print, serial_println};
 
-static FRAMEBUFFER_REQUEST: limine::FramebufferRequest = limine::FramebufferRequest::new(0);
+static FRAMEBUFFER_REQUEST: limine::request::FramebufferRequest = limine::request::FramebufferRequest::new();
 
 #[no_mangle]
 unsafe extern "C" fn _start() -> ! {
     // Ensure we got a framebuffer.
-    if let Some(framebuffer_response) = FRAMEBUFFER_REQUEST.get_response().get() {
-        if framebuffer_response.framebuffer_count < 1 {
+    if let Some(framebuffer_response) = FRAMEBUFFER_REQUEST.get_response() {
+        if framebuffer_response.framebuffers().count() < 1 {
             hcf();
         }
 
         // Get the first framebuffer's information.
-        let framebuffer = &framebuffer_response.framebuffers()[0];
+        let framebuffer_option = &framebuffer_response.framebuffers().next();
+        match framebuffer_option {
+            Some(framebuffer) => {
+                for i in 0..100_usize {
+                    // Calculate the pixel offset using the framebuffer information we obtained above.
+                    // We skip `i` scanlines (pitch is provided in bytes) and add `i * 4` to skip `i` pixels forward.
+                    let pixel_offset = i * framebuffer.pitch() as usize + i * 4;
 
-        for i in 0..100_usize {
-            // Calculate the pixel offset using the framebuffer information we obtained above.
-            // We skip `i` scanlines (pitch is provided in bytes) and add `i * 4` to skip `i` pixels forward.
-            let pixel_offset = i * framebuffer.pitch as usize + i * 4;
-
-            // Write 0xFFFFFFFF to the provided pixel offset to fill it white.
-            // We can safely unwrap the result of `as_ptr()` because the framebuffer address is
-            // guaranteed to be provided by the bootloader.
-            unsafe {
-                *(framebuffer.address.as_ptr().unwrap().offset(pixel_offset as isize) as *mut u32) = 0xFFFFFFFF;
-            }
+                    // Write 0xFFFFFFFF to the provided pixel offset to fill it white.
+                    // We can safely unwrap the result of `as_ptr()` because the framebuffer address is
+                    // guaranteed to be provided by the bootloader.
+                    unsafe {
+                        *(framebuffer.addr().offset(pixel_offset as isize) as *mut u32) = 0xFFFFFFFF;
+                    }
+                }
+            },
+            None => hcf()
         }
     }
 
